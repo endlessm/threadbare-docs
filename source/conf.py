7f01c8897e8d66b49cd9c4c175a1e0e16a80ee83
Will Thompson <wjt@endlessaccess.org>
2025-08-21 12:32:25 +0100

    Add Markdown support

--- source/conf.py
+++ source/conf.py
@@ -13,7 +13,7 @@ author = "Endless Access"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx_rtd_theme"]
+extensions = ["sphinx_rtd_theme", "myst_parser"]
 
 templates_path = ["_templates"]
 exclude_patterns = []
